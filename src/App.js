2c4278013888d4d37cc2f8f0926a29fcf000415c
Kasey Arnold <kaseyarnold15@gmail.com>
2021-02-03 18:13:42 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent.js';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent.js';
+import MouseComponent from './MouseComponent.js';
 
 class App extends Component {
 	render() {
